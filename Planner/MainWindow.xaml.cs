570292f82e257fccb434c8409e0454936ad0cf2e
Артем Мягков <artem2000m@gmail.com>
2019-12-16 16:32:07 +0300

    dhtrhrthrt

--- Planner/MainWindow.xaml.cs
+++ Planner/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Planner
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
