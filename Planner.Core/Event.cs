5fd9ca89fef5d27c05f1058eef536e92a33a96ba
Артем Мягков <artem2000m@gmail.com>
2019-12-17 18:27:54 +0300

    Добавлена библиотека классов

---
+++ Planner.Core/Event.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Planner.Core
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDt { get; set; }
+        public DateTime EndDt { get; set; }
+        public string Tag { get; set; }
+        public Event(string name, string description, DateTime startDt, DateTime endDt, string tag)
+        {
+            Name = name;
+            Description = description;
+            StartDt = startDt;
+            EndDt = endDt;
+            Tag = tag;
+        }
+    }
+}
